1c8f88be652da65235afd4215c42264ac75a65be
nurmatov0228 <bahodirnur06@gmail.com>
2024-09-24 20:35:03 -0800

    nihoyat

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import "./App.scss";
 import Layout from "./Components/Layout/Layout";
 
 const App = () => {
-  const naruto = `https://watchseriesstream.com/watch-movie/watch-the-last-naruto-the-movie-full-online-57164.5455603`;
-  console.log(naruto);
 
   return (
     <div>
